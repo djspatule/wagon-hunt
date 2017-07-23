4abf7a577c8356844d94ff0cbe079428ad454c2e
Lionel ARNAUD <djspatule@wanadoo.fr>
2017-07-23 15:14:10 +0000

    implemented edit, update and destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,15 +28,25 @@ class ProductsController < ApplicationController
     end
     
     def edit
-        
+        @product = Product.find(params[:id].to_i)
     end
     
     def update
-        
+        @product = Product.find(params[:id].to_i)
+        if @product.update(products_params)
+           redirect_to products_path
+        else
+            render :edit
+        end
     end
     
     def destroy
-        
+        @product = Product.find(params[:id].to_i)
+        if @product.destroy
+           redirect_to products_path
+        else
+            render :show
+        end
     end
     
     def products_params
